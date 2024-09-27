63a9ca166383cf59678e5499a89f1bc0f5c9618c
renribsilva <ren.rib.silva@gmail.com>
2024-09-26 21:29:39 -0300

    plugins mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
-import createMDX from '@next/mdx'
- 
+import createMDX from '@next/mdx';
+import remarkFootnotes from 'remark-footnotes';
+import remarkSmartypants from 'remark-smartypants';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
-}
- 
+};
+
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-})
- 
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [
+      [remarkFootnotes, { inlineNotes: true }], // Habilita notas de rodapé inline
+      remarkSmartypants, // Converte -- para meia-risca, etc.
+    ],
+  },
+});
+
+export default withMDX(nextConfig);
