896c063ded37ab4c4f3a064b5bbd0316b9025a07
renribsilva <ren.rib.silva@gmail.com>
2024-09-30 16:43:05 -0300

    arquivos modificados

--- src/lib/ServerConstants.ts
+++ src/lib/ServerConstants.ts
@@ -1,17 +1,18 @@
 import "dotenv/config";
 
 const normalizeId = (id) => {
-  if (!id) {return id;} 
-  if (id.length === 36) {return id;}
+  if (!id) {
+    return id;
+  }
+  if (id.length === 36) {
+    return id;
+  }
   if (id.length !== 32) {
     throw new Error(
-      "O id da base de dados é inválido: ${id} deve ter comprimento de 32 caracteres"
+      `O id da base de dados é inválido: ${id} deve ter comprimento de 32 caracteres`
     );
   }
-  return `${id.substr(0, 8)}-${id.substr(8, 4)}-${id.substr(12, 4)}-${id.substr(
-    16,
-    4
-  )}-${id.substr(20)}`;
+  return `${id.slice(0, 8)}-${id.slice(8, 12)}-${id.slice(12, 16)}-${id.slice(16, 20)}-${id.slice(20)}`;
 };
 
 export const NOTION_TOKEN = process.env.NOTION_TOKEN;
