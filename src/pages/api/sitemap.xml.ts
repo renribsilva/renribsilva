318ee447e7940ab8260360f92727bc3cf8da5c26
renribsilva <ren.rib.silva@gmail.com>
2024-10-09 11:10:20 -0300

    arquivos modificados

--- src/pages/api/sitemap.xml.ts
+++ src/pages/api/sitemap.xml.ts
@@ -51,7 +51,7 @@ const Sitemap = async (req: NextApiRequest, res: NextApiResponse) => {
   });
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">  <!-- Corrigido o namespace -->
     ${pages
       .map(({ path, lastModified }) => {
         return `
