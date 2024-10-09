a8fccd57b9426c7d0f1133eb3331d839bc59670f
renribsilva <ren.rib.silva@gmail.com>
2024-10-08 23:46:00 -0300

    arquivos modificados

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 interface HeaderProps {
   titlePre?: string;
@@ -21,16 +21,12 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
   
   const { asPath } = useRouter(); // Use asPath para obter o caminho da URL
-  const [fullUrl, setFullUrl] = useState("");
 
-  useEffect(() => {
-    const isDev = process.env.NODE_ENV === "development";
-    const baseUrl = isDev 
-      ? "http://localhost:3000" 
-      : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL || "petricor.xyz"}`;
-    setFullUrl(`${baseUrl}${asPath}`);
-  }, [asPath]); // Dependência no asPath para atualizar a URL completa
-
+  const isDev = process.env.NODE_ENV === "development";
+  const baseUrl = isDev ? "http://localhost:3000" : "https://petricor.xyz";
+  const fullUrl = `${baseUrl}${asPath}`; // Construir a URL completa
+  const imgUrl = `${baseUrl}/api/og`; 
+  
   const defaultTitle = titlePre === "Petricor" ? "Petricor" : `${titlePre} | Petricor`;
 
   const metas = {
@@ -49,8 +45,7 @@ const Header: React.FC<HeaderProps> = ({
     author_url: "https://ursal.zone/@renribsilva",
     provider_name: "Petricor",
     provider_url: "https://petricor.xyz",
-    // Usando a rota da API para a imagem
-    image: `${process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"}/api/og`,
+    image: imgUrl
   };
 
   return (
