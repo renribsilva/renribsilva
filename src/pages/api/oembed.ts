8d1a4f33716c9a93b4c495a8b128ca4870684147
renribsilva <ren.rib.silva@gmail.com>
2024-10-09 21:19:46 -0300

    arquivos modificados

--- src/pages/api/oembed.ts
+++ src/pages/api/oembed.ts
@@ -3,17 +3,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const oEmbedResponse = {
     version: "1.0",
-    type: "photo", // Especifica o tipo como "link"
+    type: "photo", 
     provider_name: "Petricor",
     provider_url: "https://petricor.xyz",
     title: "Petricor - Um Blog de Escrita",
     author_name: "renribsilva",
     author_url: "https://ursal.zone/@renribsilva",
-    url: "https://petricor.xyz", // URL que será embutida
+    url: "https://petricor.xyz",
     description: "Leia artigos e textos sobre escrita, paixão ruína e reflexão.",
-    thumbnail_url: "https://petricor.xyz/file.png", // Thumbnail associada ao link
+    thumbnail_url: "https://petricor.xyz/file.png",
     thumbnail_width: 600,
-    thumbnail_height: 400,
+    thumbnail_height: 400
   };
 
   res.status(200).json(oEmbedResponse);
