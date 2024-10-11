95b9b8e6df16f1239e7fc584f06286539075f980
renribsilva <ren.rib.silva@gmail.com>
2024-10-11 11:37:56 -0300

    arquivos modificados

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,8 @@ export default function Document() {
       <Head>
         <link 
           rel="preload"
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wdth,wght@0,75..100,300..800;1,75..100,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wdth,wght@0,75..100,300..800;1,75..100,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+        />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
