44a330e261482e6435970d121f2d884fa60c5a8b
renribsilva <ren.rib.silva@gmail.com>
2024-10-13 16:13:12 -0300

    arquivos modificados

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,87 +1,88 @@
-  // No seu componente Post
-  import { getAllPostSlugs, getPostData } from "../../lib/getMDXPosts";
-  import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
-  import { serialize } from "next-mdx-remote/serialize";
-  import MdxLayout from "../../layout/layout_mdx";
-  import Datetime from "../../components/datetime";
-  import type { PostData, PostSlug } from "../../mdxtypes";
-  import styles from "../../styles/pages.module.css";
-  import React from "react";
-  import Tagbutton from "../../components/tagbutton"; 
-  import Link from "next/link";
-  import { formatString } from "../../lib/formatString"; // Certifique-se de que o caminho esteja correto
+// pages/blog/[slug].tsx
+
+import { getAllPostSlugs, getPostData } from "../../lib/getMDXPosts";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import MdxLayout from "../../layout/layout_mdx";
+import Datetime from "../../components/datetime";
+import type { PostData, PostSlug } from "../../mdxtypes";
+import styles from "../../styles/pages.module.css";
+import React from "react";
+import Tagbutton from "../../components/tagbutton"; 
+import Link from "next/link";
+import { formatString } from "../../lib/formatString"; // Certifique-se de que o caminho esteja correto
 import Header from "../../components/header";
 import Breadcrumb from "../../components/breadcrumb";
 
-  interface PostProps {
-    postData: Omit<PostData, "content"> & {
-      content: MDXRemoteSerializeResult; // Conteúdo serializado para MDXRemote
-    };
-  }
+interface PostProps {
+  postData: Omit<PostData, "content"> & {
+    content: MDXRemoteSerializeResult; // Conteúdo serializado para MDXRemote
+  };
+}
 
-  export async function getStaticPaths() {
-    const mdfiles: PostSlug[] = getAllPostSlugs(); // Utilize PostSlug como tipo
-    return {
-      paths: mdfiles.map((file) => ({
-        params: { slug: file.params.slug },
-      })),
-      fallback: false, // Se `false`, qualquer rota não encontrada retorna 404
-    };
-  }
+export async function getStaticPaths() {
+  const mdfiles: PostSlug[] = getAllPostSlugs(); // Utilize PostSlug como tipo
+  return {
+    paths: mdfiles.map((file) => ({
+      params: { slug: file.params.slug },
+    })),
+    fallback: false, // Se `false`, qualquer rota não encontrada retorna 404
+  };
+}
 
-  export async function getStaticProps({ params }: { params: { slug: string } }) {
-    const postData: PostData = await getPostData(params.slug);
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+  const postData: PostData = await getPostData(params.slug);
 
-    // Serializa o conteúdo MDX para uso com MDXRemote
-    const mdxSource = await serialize(postData.content);
-    
-    return {
-      props: {
-        postData: {
-          ...postData,
-          content: mdxSource, // Conteúdo serializado
-        },
+  // Serializa o conteúdo MDX para uso com MDXRemote
+  const mdxSource = await serialize(postData.content);
+  
+  return {
+    props: {
+      postData: {
+        ...postData,
+        content: mdxSource, // Conteúdo serializado
       },
-    };
-  }
+    },
+  };
+}
 
-  export default function Post({ postData }: PostProps) {
-    return (
-      <MdxLayout>
-        <Header 
-          titlePre="Blog"
-          description="Texto dedicado ao aprendizado da escrita"
-          posttitle={postData.title}
-          postsubtitle={postData.subtitle}
-          posttags={postData.tags}
-        />
-        <Breadcrumb />
-        <h1 className={styles.blog_slug_title}>
-          <div>{postData.title}</div>
-          <div className={styles.blog_slug_subtitle}>
-            {postData.subtitle && postData.subtitle.length > 0 ? `${postData.subtitle}` : ""}
-          </div>
-        </h1>
-        <div className={styles.blog_slug_date}>
-          <span>
-            <Datetime date={postData.date} />
-          </span>
+export default function Post({ postData }: PostProps) {
+  return (
+    <MdxLayout>
+      <Header 
+        titlePre="Blog"
+        description="Texto dedicado ao aprendizado da escrita"
+        posttitle={postData.title}
+        postsubtitle={postData.subtitle}
+        posttags={postData.tags}
+      />
+      <Breadcrumb />
+      <h1 className={styles.blog_slug_title}>
+        <div>{postData.title}</div>
+        <div className={styles.blog_slug_subtitle}>
+          {postData.subtitle && postData.subtitle.length > 0 ? `${postData.subtitle}` : ""}
         </div>
-        <div>
-          <MDXRemote {...postData.content} />
+      </h1>
+      <div className={styles.blog_slug_date}>
+        <span>
+          <Datetime date={postData.date} />
+        </span>
+      </div>
+      <div>
+        <MDXRemote {...postData.content} />
+      </div>
+      <section className={styles.blog_slug_tags}>
+        <div className={styles.blog_slug_tags1}>
+          {postData.tags.length > 0 && <span>Tags:</span>}
         </div>
-        <section className={styles.blog_slug_tags}>
-          <div className={styles.blog_slug_tags1}>
-            {postData.tags.length > 0 && <span>Tags:</span>}
-          </div>
-          <div className={styles.blog_slug_tags2}>
-            {postData.tags.map((tag: string, index: number) => (
-              <Link href={`/tags/${formatString(tag)}`} key={index}>
-                <Tagbutton># {tag}</Tagbutton>
-              </Link>
-            ))}
-          </div>
-        </section>
-      </MdxLayout>
-    );
-  }
+        <div className={styles.blog_slug_tags2}>
+          {postData.tags.map((tag: string, index: number) => (
+            <Link href={`/tags/${formatString(tag)}`} key={index}>
+              <Tagbutton># {tag}</Tagbutton>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </MdxLayout>
+  );
+}
