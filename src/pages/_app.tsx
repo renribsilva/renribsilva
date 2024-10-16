3d2da557f9e7c3a95c1a290b372e3ca962be2ac9
renribsilva <ren.rib.silva@gmail.com>
2024-10-16 19:17:48 -0300

    arquivos modificados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,29 +7,29 @@ import { MDXProvider } from "@mdx-js/react"; // Importa o MDXProvider
 import LayoutIndex from "../layout/layout_index";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-// import { DefaultSeo } from "next-seo"; 
+import { DefaultSeo } from "next-seo"; 
 import React from "react";
 import "../styles/global.css";
 
 // Configurações de SEO padrão
-// const SEO = {
-//   title: "Petricor",
-//   description: "Blog criado com Next.js e Notion dedicado ao aprendizado da escrita",
-//   canonical: "https://petricor.xyz",
-//   openGraph: {
-//     type: "link",
-//     locale: "pt_BR",
-//     url: "https://petricor.xyz",
-//     images: [
-//       {
-//         url: "https://petricor-phi.vercel.app/api/og", 
-//         width: 800,
-//         height: 600,
-//         alt: "Imagem com fundo brando e a palavra Petricor em preto",
-//       },
-//     ],
-//   },
-// };
+const SEO = {
+  title: "Petricor",
+  description: "Blog criado com Next.js e Notion dedicado ao aprendizado da escrita",
+  canonical: "https://petricor.xyz",
+  openGraph: {
+    type: "link",
+    locale: "pt_BR",
+    url: "https://petricor.xyz",
+    images: [
+      {
+        url: "https://petricor-phi.vercel.app/api/og", 
+        width: 800,
+        height: 600,
+        alt: "Imagem com fundo brando e a palavra Petricor em preto",
+      },
+    ],
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   // Obtém os componentes personalizados
@@ -37,7 +37,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider>
-      {/* <DefaultSeo {...SEO} />  */}
+      <DefaultSeo {...SEO} /> 
       <LayoutIndex>
         <MDXProvider components={components}>
           <Component {...pageProps} />
