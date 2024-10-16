6aa5377037f1b86d54047e486c6bfe0abbf340ab
renribsilva <ren.rib.silva@gmail.com>
2024-10-16 15:25:26 -0300

    arquivos modificados

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -5,56 +5,56 @@ export const config = {
   runtime: "edge",
 };
 
+const raleway = fetch(
+  new URL("../../../public/Raleway-ExtraLight.ttf", import.meta.url)
+)
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error(`Failed to fetch font: ${res.statusText}`);
+    }
+    return res.arrayBuffer();
+  });
+
 export default async function handler() {
-  // Defina a URL base de acordo com o ambiente
-  const baseUrl = process.env.NODE_ENV === "production" 
-    ? "https://petricor-phi.vercel.app" 
-    : "http://localhost:3000";
+  const ralewayData = await raleway;
 
-  // Crie a URL para a imagem no diretório public
-  const imageUrl = new URL("/file.png", baseUrl).toString();
+  const { title } = {
+    title: "Petricor",
+  };
 
-  // Fetch a imagem
-  const res = await fetch(imageUrl);
-
-  // Verifique se a resposta é válida
-  if (!res.ok) {
-    return new Response("Failed to load image", { status: 500 });
-  }
-
-  // Obtenha os dados da imagem como um buffer
-  const imageBuffer = await res.arrayBuffer(); 
-
-  // Converta o buffer em Base64
-  const base64Image = Buffer.from(imageBuffer).toString("base64");
-
-  // Retorne a resposta da imagem
   return new ImageResponse(
     (
       <div
         style={{
-          width: "100%",
           height: "100%",
+          width: "100%",
           display: "flex",
-          justifyContent: "center",
+          flexDirection: "column",
           alignItems: "center",
-          background: "#000000",
+          justifyContent: "center",
+          backgroundColor: "#FFFFFF", // cor de fundo em hexadecimal
+          fontFamily: "Raleway",
         }}
       >
-        <img 
-          src={`data:image/png;base64,${base64Image}`} 
-          alt="File Image" 
+        <h1
           style={{
-            maxWidth: "100%",
-            maxHeight: "100%",
-            objectFit: "contain",
+            fontSize: 200,
+            color: "#272728", // cor do texto em hexadecimal
           }}
-        />
+        >
+          {title}
+        </h1>
       </div>
     ),
     {
       width: 1200,
       height: 630,
-    },
+      fonts: [
+        {
+          name: "Raleway",
+          data: ralewayData,
+        },
+      ],
+    }
   );
 }
