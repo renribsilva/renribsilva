7e1a96955377bf211823a5e2c632a3c579198719
renribsilva <ren.rib.silva@gmail.com>
2024-10-16 20:32:06 -0300

    arquivos modificados

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,10 @@
-import { Html, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
+      <Head />
       <body>
         <Main />
         <NextScript />
