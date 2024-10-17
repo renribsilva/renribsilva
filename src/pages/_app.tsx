77c6019d22ae82a750414dc8137597fca7892320
renribsilva <ren.rib.silva@gmail.com>
2024-10-16 22:43:46 -0300

    arquivos modificados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,17 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
 import React from "react";
+import { useRouter } from "next/router";
 import "../styles/global.css";
 
 // Defina a URL do host para as imagens padrão
-const url = "https://petricor.xyz";
+const urlbase = "https://petricor.xyz";
 
 export default function App({ Component, pageProps }: AppProps) {
   // Obtém os componentes personalizados para MDX
   const components = useMDXComponents({});
+  const router = useRouter();
+  const url = `${urlbase}${router.asPath}`;
 
   // Configuração dos metadados dinâmicos
   const title = pageProps.title ? pageProps.title : "Petricor";
@@ -23,16 +26,15 @@ export default function App({ Component, pageProps }: AppProps) {
     : "Blog criado com Next.js e Notion dedicado ao aprendizado da escrita.";
   const image = pageProps.image
     ? pageProps.image
-    : `${url}/api/og`;
+    : `${urlbase}/api/og`;
 
   // Configuração dos metadados estáticos
   const type = "link";
   const site_name = "Petricor";
 
   return (
-    <ThemeProvider>
+    <ThemeProvider attribute="class">
       <Head>
-
         {/*metas globais*/}
         <meta name="title" content={title} />
         <meta name="description" content={description} />
@@ -53,7 +55,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
         {/*canonical*/}
         <link rel="canonical" href={url} />
-
       </Head>
       <LayoutIndex>
         <MDXProvider components={components}>
