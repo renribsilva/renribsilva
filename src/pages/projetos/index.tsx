1766491fa3b2d25a3ae1f22b60a5f61ab63255ca
renribsilva <ren.rib.silva@gmail.com>
2024-10-27 15:05:13 -0300

    arquivos modificados

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Conjugador from "../../components/mdx/conjugador.mdx";
 import Breadcrumb from "../../components/breadcrumb";
+import Header from "../../components/header";
+
+// Função para obter título e descrição
+export const getStaticProps = async () => {
+  // Simulando a recuperação do título e da descrição
+  const ogtitle = "Projetos | Petricor"; // Substitua conforme necessário
+  const ogdescription = "Projetos que estão sendo escritos pelo autor do blog"; // Substitua conforme necessário
+  
+  return {
+    props: {
+      ogtitle, // Exporta o título
+      ogdescription, // Exporta a descrição
+    },
+  };
+};
 
 export default function Projetos () {
   return (
     <section>
+      <Header titlePre="Projetos" />
       <Breadcrumb />
       <Conjugador />
     </section>
