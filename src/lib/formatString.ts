32c139105eca31776cf584993dc577c848670b6a
renribsilva <ren.rib.silva@gmail.com>
2024-12-08 13:24:20 -0300

    arquivos modificados

--- src/lib/formatString.ts
+++ src/lib/formatString.ts
@@ -5,5 +5,5 @@ export function formatString(input: string): string {
     .replace(/\s+/g, "-") // Substitui espaços por hífens
     .replace(/[\u0300-\u036f]/g, "") // Remove acentos e diacríticos
     .replace(/--+/g, "-") // Substitui múltiplos hífens por um único
-    .trim(); // Remove espaços em branco extras
+    .trim(); // Remove espaços em branco extras 
 }
