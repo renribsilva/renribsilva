698be947688d2d230b82a437134f9dcf0f9d8b4d
renribsilva <ren.rib.silva@gmail.com>
2024-12-17 12:06:19 -0300

    arquivos modificados

--- src/pages/arquivo/tag/index.tsx
+++ src/pages/arquivo/tag/index.tsx
@@ -1,16 +1,14 @@
 import { GetServerSideProps } from "next";
 
-// Componente de redirecionamento (não vai renderizar nada)
 const RedirectIndex = () => {
   return null;
 };
 
-// Função getServerSideProps que redireciona para /arquivo
 export const getServerSideProps: GetServerSideProps = async () => {
   return {
     redirect: {
-      destination: "/arquivo", // Redireciona para /arquivo
-      permanent: false, // Redirecionamento temporário
+      destination: "/arquivo", 
+      permanent: false, 
     },
   };
 };
