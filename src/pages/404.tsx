9305687524999d597d41b264758d84ad7813d2fa
renribsilva <ren.rib.silva@gmail.com>
2025-08-23 11:45:42 -0300

    arquivos modificados

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Custom404() {
-  return <h1>Que pena</h1>;
+  return (
+    <section>
+      <h1>Eita!!!</h1>
+      <p>A página que você procura não foi encontrada. Para encontrar a página que deseja,
+        navegue no menu acima.
+      </p>
+    </section>
+  );
 }
