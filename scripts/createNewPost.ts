b054cb469b82503e7e9408d2977bee1697e6568d
renribsilva <ren.rib.silva@gmail.com>
2025-09-02 13:52:55 -0300

    arquivos modificados

--- scripts/createNewPost.ts
+++ scripts/createNewPost.ts
@@ -3,26 +3,18 @@ import matter from "gray-matter";
 import path from "path";
 import { formatString } from "../src/lib/formatString"; 
 
-// Obter números dos arquivos
-const getFileNames = async () => {
-  const postsDirectory = path.join(process.cwd(), "src/content");
-  const postFiles = fs.readdirSync(postsDirectory).filter(fileName => fileName.startsWith("post") && fileName.endsWith(".mdx"));
-  
-  return postFiles.map(fileName => fileName.match(/post(\d+)/)?.[1]).filter(Boolean);
-};
+const postsDirectory = path.join(process.cwd(), "src/content");
 
-// Obter próximo nome de arquivo
 const getNextPostFilename = async () => {
-  const postNumbers = await getFileNames();
+  const postNumbers = fs
+    .readdirSync(postsDirectory)
+    .filter(fileName => fileName.startsWith("post") && fileName.endsWith(".mdx"))
+    .map(fileName => fileName.match(/post(\d+)/)?.[1])
+    .filter(Boolean);
   const maxPostNumber = postNumbers.length ? Math.max(...postNumbers.map(Number)) : 0; 
   return `post${maxPostNumber + 1}.mdx`;
 };
 
-// Gerar ID e slug
-const generateIdFromFileName = (fileName) => fileName.replace(/\.mdx$/, "");
-const generateSlugFromTitle = (title) => formatString(title); 
-
-// Obter data local em UTC (São Paulo)
 const getLocalDateInUTC = () => {
   const localDate = new Date();
   const utcOffset = -3; // UTC-3
@@ -31,11 +23,11 @@ const getLocalDateInUTC = () => {
 
 // Função que cria novo arquivo MDX
 const createMDXFile = async (frontmatterData, content) => {
+  
   const fileName = await getNextPostFilename();
   const filePath = path.join(process.cwd(), "src/content", fileName);
-  const id = generateIdFromFileName(fileName);
-  const slug = generateSlugFromTitle(frontmatterData.title);
-
+  const id = fileName.replace(/\.mdx$/, "");
+  const slug = formatString(frontmatterData.title);
   const frontmatterString = matter.stringify(content, {
     id: String(id),
     slug: String(slug),
