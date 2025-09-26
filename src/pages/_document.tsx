9fd1ccf16efd0103c793b8f9216738b4bc3c60b0
renribsilva <ren.rib.silva@gmail.com>
2025-09-25 21:22:07 -0300

    arquivos modificados

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        />
       </Head>
       <body>
         <Main />
