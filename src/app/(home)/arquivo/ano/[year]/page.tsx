f4e85c2474920c6cd6d53782194b5a8db38f45c5
renato <ren.rib.silva@gmail.com>
2026-04-18 15:42:41 -0300

    arquivos modificados

--- src/app/(home)/arquivo/ano/[year]/page.tsx
+++ src/app/(home)/arquivo/ano/[year]/page.tsx
@@ -1,6 +1,9 @@
 import { getSortedPostsData } from "../../../../../lib/getMDXPosts";
 import PostList from "../../../../../components/tsx/post_list";
+import ArchiveButton from "../../../../../components/tsx/archive_button";
+import Link from "next/link";
 import { Post } from "../../../../../types";
+import styles from "../../arquivo.module.css"
 
 export default async function AnoPage({ params }: { params: { year: string } }) {
   
@@ -9,12 +12,19 @@ export default async function AnoPage({ params }: { params: { year: string } }) 
   const filteredPosts = allPosts.filter((post) => post.date.slice(0, 4) === year);
 
   return (
-    <section>
-      {filteredPosts.length > 0 ? (
-        <PostList posts={filteredPosts} />
-      ) : (
-        <p>Nenhum post encontrado para o ano {year}.</p>
-      )}
+    <section className={styles.arquivo_button_container}>
+      <div>
+        {filteredPosts.length > 0 ? (
+          <PostList posts={filteredPosts} />
+        ) : (
+          <p>Nenhum post encontrado para o ano {year}.</p>
+        )}
+      </div>
+      <div>
+        <ArchiveButton className={styles.back_to_archive}>
+          <Link href="/arquivo">←</Link>
+        </ArchiveButton>
+      </div>
     </section>
   );
 }
--- src/app/(home)/arquivo/tag/[tag]/page.tsx
+++ src/app/(home)/arquivo/tag/[tag]/page.tsx
@@ -1,7 +1,10 @@
 import { getSortedPostsData } from "../../../../../lib/getMDXPosts";
 import PostList from "../../../../../components/tsx/post_list";
+import ArchiveButton from "../../../../../components/tsx/archive_button";
+import Link from "next/link";
 import { Post } from "../../../../../types";
 import { formatString } from "../../../../../lib/formatString";
+import styles from "../../arquivo.module.css"
 
 export default async function TagPage({ params }: { params: { tag: string } }) {
   
@@ -12,12 +15,19 @@ export default async function TagPage({ params }: { params: { tag: string } }) {
   );
 
   return (
-    <section>
-      {filteredPosts.length > 0 ? (
-        <PostList posts={filteredPosts} />
-      ) : (
-        <p>Nenhum post encontrado para a tag {tag}.</p>
-      )}
+    <section className={styles.arquivo_button_container}> 
+      <div>
+        {filteredPosts.length > 0 ? (
+          <PostList posts={filteredPosts} />
+        ) : (
+          <p>Nenhum post encontrado para a tag {tag}.</p>
+        )}
+      </div>
+      <div>
+        <ArchiveButton className={styles.back_to_archive}>
+          <Link href="/arquivo">←</Link>
+        </ArchiveButton>
+      </div>
     </section>
   );
 }
