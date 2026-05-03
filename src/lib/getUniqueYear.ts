096cb0f27c9774b02e12f17963888ee9d8f56ad7
Renato <ren.rib.silva@gmail.com>
2026-05-03 16:43:21 -0300

    arquivos modificados

--- src/lib/getUniqueYear.ts
+++ src/lib/getUniqueYear.ts
@@ -1,9 +1,11 @@
 import { getSortedPostsData } from "./getMDXPosts";
 
 export function getUniqueYears() {
-  
+
   const posts = getSortedPostsData();
   const years = [...new Set(posts.map(post => post.date.slice(0, 4)))].sort((a, b) => b - a);
-
+  // console.log(years)
   return years;
 }
+
+// getUniqueYears()
